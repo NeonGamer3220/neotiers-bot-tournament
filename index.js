383dd80f3de57eb71d5c636efd8d38912d8f46e6
NeonGamer3220 <neon.gamer322@gmail.com>
2026-04-29 16:09:28 +0200

    Move ReadableStream polyfill to top to fix undici error

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
 // Polyfill for ReadableStream to fix undici issues in older Node versions
 const { ReadableStream } = require('node:stream/web');
 global.ReadableStream = ReadableStream;
+
+require('dotenv').config();
 const { Client, GatewayIntentBits, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, StringSelectMenuBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ChannelType } = require('discord.js');
 const { createClient } = require('@supabase/supabase-js');
 
